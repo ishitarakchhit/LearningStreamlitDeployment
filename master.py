0b81a31d01f9db4418827894bdc0785344503a80
Ishita Rakchhit <104856237+ishitarakchhit@users.noreply.github.com>
2023-07-12 18:52:51 +0530

    Create master.py

---
+++ master.py
@@ -0,0 +1,5 @@
+import streamlit as st
+
+st.header("ML and Python workshop")
+st.title("Hello learners")
+st.write("This is a part of my project that I am learning in python and ML workshop organised by AI club")
